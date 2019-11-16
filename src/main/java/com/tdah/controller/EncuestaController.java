2a801f6550f0ff0c1fe36e0c38c5cd68d3ded58c
abelkevin18 <abel.nunez.27@unsch.edu.pe>
2019-11-16 10:34:59 -0500

    registrar encuesta v3

--- src/main/java/com/tdah/controller/EncuestaController.java
+++ src/main/java/com/tdah/controller/EncuestaController.java
@@ -1,16 +1,25 @@
 package com.tdah.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.tdah.model.Encuesta;
+import com.tdah.model.Estudiante;
 import com.tdah.model.InstitucionEducativa;
 import com.tdah.service.IEncuestaService;
 import com.tdah.service.IInstitucionEducativaService;
@@ -30,18 +39,38 @@ public class EncuestaController {
 	IEncuestaService encuestaService;
 	
 	@GetMapping("/registrar")
-	public ModelAndView registrarEncuesta() {
+	public ModelAndView vistaEncuesta() {
 		log.info("registrar encuesta");
 		List<InstitucionEducativa> institucionEducativas = institucionEducativaService.findAll();
 		
 		List<Encuesta> encuestas = encuestaService.findAll();
-		
 		List<Encuesta> encuestasEnproceso = encuestas.stream().filter(e -> e.getEstado().equalsIgnoreCase("EN PROCESO")).collect(Collectors.toList());	
+		
+		Encuesta encuesta = new Encuesta();
 		
 		ModelAndView model = new ModelAndView("encuesta/registrar-encuesta");		
 		model.addObject("institucionEducativas", institucionEducativas);
 		model.addObject("encuestasEnproceso", encuestasEnproceso);
+		model.addObject("encuesta", encuesta);
 		return model;
+	}
+	
+	@PostMapping("/registrar")
+	public String registrarEncuesta(@Valid Encuesta encuesta, BindingResult result, Model model, RedirectAttributes flash, SessionStatus status) {
+	
+		log.info("registrar encuesta");
+		log.info(""+ encuesta.getInstitucionEducativa().getDenominacion());
+		
+		encuesta.setEstado("EN PROCESO");
+		encuesta.setFechaCreacion(new Date());
+		
+		try {
+			encuestaService.saveOrUpdate(encuesta);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "redirect:/";
 	}
 	
 	
