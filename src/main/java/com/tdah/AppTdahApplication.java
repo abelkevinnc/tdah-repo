c5ddb12634a96660e912643d0ef4fa4afb12ce8b
abel <abelkevin15@gmail.com>
2020-08-29 22:47:47 -0500

    command line

--- src/main/java/com/tdah/AppTdahApplication.java
+++ src/main/java/com/tdah/AppTdahApplication.java
@@ -1,13 +1,24 @@
 package com.tdah;
 
+import com.tdah.model.Estudiante;
+import com.tdah.service.IEstudianteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class AppTdahApplication {
+public class AppTdahApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppTdahApplication.class, args);
 	}
 
+
+
+	@Override
+	public void run(String... args) throws Exception {
+	}
 }
